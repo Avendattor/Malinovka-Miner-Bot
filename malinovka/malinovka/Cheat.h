380d91b6df27e3eef7cc72baecdaab6ce60f5a71
liefasm <liefasm@gmail.com>
2019-10-13 22:33:15 +0300

    Update Cheat.h

--- malinovka/malinovka/Cheat.h
+++ malinovka/malinovka/Cheat.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "Player.h"
 
-#define CPed								0xB6F5F0
+#define CPed					0xB6F5F0
 #define MIDDLE_VERTICAL_CAMERA_POSITION		-0.326023f
-# define M_PI								3.14159265358979323846
+# define M_PI					3.14159265358979323846
 
 class Cheat
 {
