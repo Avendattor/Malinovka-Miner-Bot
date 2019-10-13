c514720595a2ed1fdcc4160b4ef44029e33eef29
liefasm <liefasm@gmail.com>
2019-10-13 22:32:12 +0300

    Update MinerBot.h

--- malinovka/malinovka/MinerBot.h
+++ malinovka/malinovka/MinerBot.h
@@ -3,8 +3,8 @@
 class MinerBot : public Cheat
 {
 private:
-		float X = 1223.22f, Y = 825.408f, Z = 1002.0f; // Êîîðäèíàòû
-		int status = 0;
+	float X = 1223.22f, Y = 825.408f, Z = 1002.0f; // Êîîðäèíàòû
+	int status = 0;
 public:
 	void run();
 	MinerBot();
